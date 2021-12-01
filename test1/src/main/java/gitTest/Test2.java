0afd6b26834f61605b291a1d67a4da93807ea924
Dajeong <ekwjd8114@naver.com>
2021-12-01 12:16:59 +0900

    test commit

---
+++ test1/src/main/java/gitTest/Test2.java
@@ -0,0 +1,7 @@
+package gitTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Test2클래스를 ks 브랜치에서 작업함");
+	}
+}
--- test1/src/main/java/gitTest/TestMain.java
+++ test1/src/main/java/gitTest/TestMain.java
@@ -1,7 +1,8 @@
-package test1;
+package gitTest;
 
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("새로운 브랜치 (dj)에서 작업함");
 	}
 }
